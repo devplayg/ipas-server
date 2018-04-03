b12ecfa04434278ff331e49d331a23fe72b2c4ba
devplayg <devplayg@gmail.com>
2018-04-03 17:52:12 +0900

    added sorter removed duplicate logic

---
+++ sorter/sorter.go
@@ -0,0 +1,49 @@
+package sorter
+
+import (
+	"github.com/devplayg/ipas-server"
+	log "github.com/sirupsen/logrus"
+	"github.com/fsnotify/fsnotify"
+)
+
+type Sorter struct {
+	engine *ipasserver.Engine
+}
+
+
+func NewSorter(engine *ipasserver.Engine) *Sorter {
+	return &Sorter{
+		engine: engine,
+	}
+}
+
+func (s *Sorter) Start() error {
+	//watcher, err := fsnotify.NewWatcher()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer watcher.Close()
+	//
+	//done := make(chan bool)
+	//go func() {
+	//	for {
+	//		select {
+	//		case event := <-watcher.Events:
+	//			log.Println("event:", event)
+	//			if event.Op&fsnotify.Write == fsnotify.Write {
+	//				log.Println("modified file:", event.Name)
+	//			}
+	//		case err := <-watcher.Errors:
+	//			log.Println("error:", err)
+	//		}
+	//	}
+	//}()
+	//
+	//err = watcher.Add("/tmp/foo")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//<-done
+
+	return nil
+}
