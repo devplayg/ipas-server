db78bed1dfab5ab3d061b7ce2f65102f9dc50eaf
devplayg <devplayg@gmail.com>
2018-04-16 22:37:32 +0900

    setup calculator framework

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -4,7 +4,20 @@ import (
 	"github.com/devplayg/ipas-server"
 	log "github.com/sirupsen/logrus"
 	"strings"
+	"sync"
 	"time"
+)
+
+type IpasEvent struct {
+	orgId     int
+	groupId   int
+	eventType int
+	equipId   string
+	targets   string
+}
+
+const (
+	RootId = -1
 )
 
 type Calculator struct {
@@ -13,6 +26,7 @@ type Calculator struct {
 	interval int64
 	date     string
 	report   string
+	assetMap map[int][]int
 }
 
 func NewCalculator(engine *ipasserver.Engine, top int, interval int64, date, report string) *Calculator {
@@ -25,19 +39,13 @@ func NewCalculator(engine *ipasserver.Engine, top int, interval int64, date, rep
 	}
 }
 
-func (c *Calculator) calculate(from, to, mark string) error {
-	log.Debugf("[%s ~ %s] Mark as %s", from, to, mark)
-	return nil
-}
-
 func (c *Calculator) Start() error {
-	log.Debug("start")
 	if len(c.date) > 0 { // 특정 지정한 날짜에 대한 통계 생성
 		t, err := time.Parse("2006-01-02", c.date)
 		if err != nil {
 			return err
 		}
-		log.Debugf("Target date: %s", c.date)
+		log.Debugf("Calculating statistics for %s", c.date)
 		err = c.calculate(
 			t.Format("2006-01-02")+" 00:00:00",
 			t.Format("2006-01-02")+" 23:59:59",
@@ -59,7 +67,7 @@ func (c *Calculator) Start() error {
 		if err != nil {
 			return err
 		}
-		log.Debugf("Target date: %s", c.report)
+		log.Debugf("Calculating statistics for %s", c.report)
 		c.calculate(
 			from.Format("2006-01-02")+" 00:00:00",
 			to.Format("2006-01-02")+" 00:00:00",
@@ -88,18 +96,163 @@ func (c *Calculator) Start() error {
 	return nil
 }
 
-//type Calculator interface {
-//	Start() error
-//}
+func (c *Calculator) calculate(from, to, mark string) error {
+	var err error
+	c.assetMap, err = c.getMemberAssets()
+	if err != nil {
+		log.Error(err)
+	}
 
-//func NewCalculator(engine *ipasserver.Engine, logType, top int, interval int64, date, manual string) Calculator {
-//	if logType == objs.LogEvent {
-//		return NewEventCalculator(engine, top, interval, date, manual)
-//
-//	} else if logType == objs.StatusEvent {
-//		return NewStatusCalculator(engine, top, interval, date, manual)
-//
-//	} else {
-//		return nil
+	start := time.Now()
+	log.Debugf("Calculating statistics, [%s ~ %s] Mark as %s", from, to, mark)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go c.calculateEvents(wg, from, to, mark)
+	wg.Add(1)
+	go c.calculateStatus(wg, from, to, mark)
+	wg.Wait()
+
+	// Write signature
+	log.Debugf("Done. Execution time: %3.1f", time.Since(start).Seconds())
+	return nil
+}
+
+func (c *Calculator) calculateEvents(wg *sync.WaitGroup, from, to, mark string) error {
+	defer wg.Done()
+	log.Debug("Calculating event..")
+
+	//var (
+	//	orgId     int
+	//	groupId   int
+	//	eventType int
+	//	equipId   string
+	//	targets   string
+	//)
+
+	query := `
+		select  org_id, group_id, event_type, equip_id, targets
+		from log_ipas_event
+		where date between ? and ?
+		limit 10
+	`
+	//logs := make([]IpasEvent, 0)
+	rows, err := c.engine.DB.Query(query, from, to)
+	if err != nil {
+		log.Error(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		e := IpasEvent{}
+		err := rows.Scan(&e.orgId, &e.groupId, &e.eventType, &e.equipId, &e.targets)
+		if err != nil {
+			log.Error(err)
+		}
+
+		//logs = append(logs, e)
+		//log.Debugf("%s\t%s", equipId, targets)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Error(err)
+	}
+
+	return nil
+}
+
+func (c *Calculator) calculateStatus(wg *sync.WaitGroup, from, to, mark string) error {
+	defer wg.Done()
+	log.Debug("Calculating status..")
+	//time.Sleep(2 * time.Second)
+	return nil
+}
+
+func (c *Calculator) addToStats(r *IpasEvent, category string, val interface{}) error {
+
+//	// By sensor
+//	if r.SensorId > 0 {
+//		if _, ok := f.dataMap[r.SensorId]; !ok {
+//			f.dataMap[r.SensorId] = make(map[string]map[interface{}]int64)
+//			f._rank[r.SensorId] = make(map[string]stats.ItemList)
+//		}
+//		if _, ok := f.dataMap[r.SensorId][category]; !ok {
+//			f.dataMap[r.SensorId][category] = make(map[interface{}]int64)
+//			f._rank[r.SensorId][category] = nil
+//		}
+//		f.dataMap[r.SensorId][category][val] += 1
 //	}
-//}
+//
+//	// By group
+//	if r.IppoolSrcGcode > 0 {
+//		if _, ok := f.dataMap[r.IppoolSrcGcode]; !ok {
+//			f.dataMap[r.IppoolSrcGcode] = make(map[string]map[interface{}]int64)
+//			f._rank[r.IppoolSrcGcode] = make(map[string]stats.ItemList)
+//		}
+//		if _, ok := f.dataMap[r.IppoolSrcGcode][category]; !ok {
+//			f.dataMap[r.IppoolSrcGcode][category] = make(map[interface{}]int64)
+//			f._rank[r.IppoolSrcGcode][category] = nil
+//		}
+//		f.dataMap[r.IppoolSrcGcode][category][val] += 1
+//	}
+//
+//	// To all
+//	if _, ok := f.dataMap[RootId][category]; !ok {
+//		f.dataMap[RootId][category] = make(map[interface{}]int64)
+//		f._rank[RootId][category] = nil
+//	}
+//	f.dataMap[RootId][category][val] += 1
+//
+//	// By member
+//	if arr, ok := f.memberAssets[r.IppoolSrcGcode]; ok {
+//		for _, memberId := range arr {
+//			id := memberId * -1
+//
+//			if _, ok := f.dataMap[id]; !ok {
+//				f.dataMap[id] = make(map[string]map[interface{}]int64)
+//				f._rank[id] = make(map[string]stats.ItemList)
+//			}
+//			if _, ok := f.dataMap[id][category]; !ok {
+//				f.dataMap[id][category] = make(map[interface{}]int64)
+//				f._rank[id][category] = nil
+//			}
+//			f.dataMap[id][category][val] += 1
+//		}
+//	}
+//
+	return nil
+}
+
+
+func (c *Calculator) getMemberAssets() (map[int][]int, error) {
+	m := make(map[int][]int)
+	var (
+		memberId int
+		assetId int
+	)
+	query := `
+		select member_id, asset_id
+		from mbr_asset
+		where member_id in (select member_id from mbr_member where position < 256)
+	`
+	rows, err := c.engine.DB.Query(query)
+	if err != nil {
+		log.Error(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&memberId, &assetId)
+		if err != nil {
+			log.Error(err)
+		}
+
+		if _, ok := m[assetId]; !ok {
+			m[assetId] = make([]int, 0)
+		}
+		m[assetId] = append(m[assetId], memberId)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Error(err)
+	}
+
+	return m, nil
+}
