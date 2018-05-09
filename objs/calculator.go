5aa4cd10681a684f249ccdf8e684ab89d33c1741
devplayg <devplayg@gmail.com>
2018-05-09 15:41:15 +0900

    added http socket for stats. comment socket for stats.

--- objs/calculator.go
+++ objs/calculator.go
@@ -1,13 +1,14 @@
 package objs
 
-import "sort"
+import (
+	"sort"
+)
 
 const (
 	RealtimeCalculator     = 1
 	SpecificDateCalculator = 2
 	DateRangeCalculator    = 3
 )
-
 
 type Item struct {
 	Key   interface{}
@@ -18,9 +19,32 @@ type ItemList []Item
 type DataMap map[int]map[string]map[interface{}]int64 // Code / Category / Key / Count
 type DataRank map[int]map[string]ItemList             // Code / Category / Key / Ranking
 
-func (p ItemList) Len() int           { return len(p) }
-func (p ItemList) Less(i, j int) bool { return p[i].Count < p[j].Count }
-func (p ItemList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ItemList) Len() int { return len(p) }
+func (p ItemList) Less(i, j int) bool {
+	//return p[i].Count < p[j].Count  // && p[i].Key < p[j].Key
+	switch p[i].Key.(type) {
+	case int:
+		if p[i].Count < p[j].Count {
+			return true
+		} else if p[i].Count == p[j].Count {
+			return p[i].Key.(int) < p[j].Key.(int)
+		} else {
+			return false
+		}
+	case string:
+		if p[i].Count < p[j].Count {
+			return true
+		} else if p[i].Count == p[j].Count {
+			return p[i].Key.(string) > p[j].Key.(string)
+		} else {
+			return false
+		}
+	default:
+		return p[i].Count < p[j].Count
+	}
+}
+
+func (p ItemList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
 
 func DetermineRankings(m map[interface{}]int64, top int) ItemList {
 	list := make(ItemList, len(m))
