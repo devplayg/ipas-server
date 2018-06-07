47593560d5d490a692e565137726c15ec8323dad
devplayg <devplayg@gmail.com>
2018-06-07 21:50:33 +0900

    added option to script

--- cmd/build/service/app.sh
+++ cmd/build/service/app.sh
@@ -2,37 +2,41 @@
 
 dir=/root/go/src/github.com/devplayg/ipas-server/cmd/build
 
-
 addToService()
 {
     command=$1
     dir=$2
-    sed 's|###COMMAND###|'"$command"'|g; s|###DIR###|'"$dir"'|g' init.script > /etc/init.d/${command}
+    opt=$3
+    sed 's|###COMMAND###|'"$command"'|g; s|###DIR###|'"$dir"'|g; s|###OPT###|'"$opt"'|g;' init.script > /etc/init.d/${command}
     chmod 755 /etc/init.d/${command}
     chkconfig $command on
-    service $command restart
 }
 
-start()
-{
-    service start receiver
-    service start classifier
-    service start calculator
-    service start generator
-}
-
-stop()
-{
-    service stop receiver
-    service stop classifier
-    service stop calculator
-    service stop generator
-}
+mode=$1
 
 
+case "$mode" in
+    'install')
+        addToService receiver ${dir}
+        addToService classifier ${dir}
+        addToService calculator ${dir}
+        addToService "generator" ${dir} "-loop -interval 60s"
+        ;;
+
+    'start')
+    service receiver start
+    service classifier start 
+    service calculator start 
+    service generator start 
+        ;;
+
+    'stop')
+
+    service receiver stop
+    service classifier stop
+    service calculator stop
+    service generator stop
+        ;;
+esac
 
 
-addToService receiver ${dir}
-addToService classifier ${dir}
-addToService calculator ${dir}
-addToService generator ${dir}
